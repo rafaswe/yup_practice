566267159b8f604bee1c1c81fab9c4f104954424
rafaswe <49919918+rafaswe@users.noreply.github.com>
2023-09-05 16:26:37 +0600

    added optional validation

--- component/MyForm.tsx
+++ component/MyForm.tsx
@@ -31,7 +31,15 @@ const schema = yup.object().shape({
       return numericValue.length <= 11;
     })
     .required("Phone number is required"),
-
+  maritalStatus: yup
+    .mixed()
+    .oneOf(["married", "unmarried"] as const)
+    .defined(),
+  spouseName: yup.string().when(["maritalStatus"], {
+    is: "married",
+    then: (schema) => schema.required("fsdfsf"),
+    otherwise: (schema) => schema.optional(),
+  }),
   age: yup
     .number()
     .typeError("Age should be a number")
@@ -65,15 +73,34 @@ const MyForm = () => {
     handleSubmit,
     reset,
     formState: { errors },
+    clearErrors,
+    control,
+    watch,
   } = useForm({
     resolver: yupResolver(schema),
+    defaultValues: { maritalStatus: "unmarried" },
   });
 
   const onSubmit = (data: any) => {
     reset();
     setData(data);
+    console.log({ errors });
   };
-  console.log(typeof data);
+
+  const clearAllError = () => {
+    clearErrors([
+      "firstname",
+      "lastName",
+      "age",
+      "email",
+      "maritalStatus",
+      "spouseName",
+      "phone",
+      "password",
+      "confirmPassword",
+    ]);
+  };
+  console.log("Marital Status = " + watch("maritalStatus"));
 
   return (
     <div className="form">
@@ -82,27 +109,80 @@ const MyForm = () => {
       <form onSubmit={handleSubmit(onSubmit)} className="form-body">
         <div className="col-6">
           <label>First Name</label>
-          <input type="text" {...register("firstname")} />
+          <input
+            type="text"
+            {...register("firstname")}
+            onChange={() => clearAllError()}
+          />
           <p className="error-message">{errors.firstname?.message}</p>
         </div>
         <div className="col-6">
           <label>Last Name</label>
-          <input type="text" {...register("lastName")} />
+          <input
+            type="text"
+            {...register("lastName")}
+            onChange={() => clearAllError()}
+          />
           <p className="error-message">{errors.lastName?.message}</p>
         </div>
         <div className="col-6">
           <label>Email</label>
-          <input type="text" {...register("email")} />
+          <input
+            type="text"
+            {...register("email")}
+            onChange={() => clearAllError()}
+          />
           <p>{errors.email?.message}</p>
         </div>
         <div className="col-6">
+          <label className="pb">Marital Status:</label>
+          <div className="flex-box">
+            <label className="flex-box">
+              <input
+                type="radio"
+                value="married"
+                {...register("maritalStatus")}
+                onClick={() => clearAllError()}
+              />{" "}
+              Married
+            </label>
+            <label className="flex-box">
+              <input
+                type="radio"
+                value="unmarried"
+                {...register("maritalStatus")}
+                onClick={() => clearAllError()}
+              />{" "}
+              Unmarried
+            </label>
+          </div>
+          {errors.maritalStatus && <p>{errors.maritalStatus.message}</p>}
+        </div>
+        {watch("maritalStatus") === "married" && (
+          <div className="col-6">
+            <label>Spouse Name</label>
+            <input type="text" {...register("spouseName")} />
+            <p>{errors.spouseName?.message}</p>
+          </div>
+        )}
+
+        <div className="col-6">
           <label>Phone</label>
-          <input type="text" {...register("phone")} />
+          <input
+            type="text"
+            {...register("phone")}
+            onChange={() => clearAllError()}
+          />
           <p>{errors.phone?.message}</p>
         </div>
+
         <div className="col-6">
           <label>Age</label>
-          <input type="number" {...register("age")} />
+          <input
+            type="number"
+            {...register("age")}
+            onChange={() => clearAllError()}
+          />
           <p>{errors.age?.message}</p>
         </div>
         <div className="col-6">
@@ -112,7 +192,11 @@ const MyForm = () => {
         </div>
         <div className="col-6">
           <label>Confirm Password</label>
-          <input type="password" {...register("confirmPassword")} />
+          <input
+            type="password"
+            {...register("confirmPassword")}
+            onChange={() => clearAllError()}
+          />
           <p>{errors.confirmPassword?.message}</p>
         </div>
 
